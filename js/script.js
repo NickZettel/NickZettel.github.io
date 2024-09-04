1d01b2339161e9f45446fa0a887ce0aa8389dd30
NickZettel <Nzettel21@gmail.com>
2024-09-04 17:24:19 -0400

    upgraded the background effect to fade when distant from mouse.

--- js/script.js
+++ js/script.js
@@ -16,45 +16,56 @@ resizeCanvas();
 //ready for resizing
 window.addEventListener('resize',resizeCanvas)
 
-let mouseX, mouseY;
+let mouse = {
+    x: 0,
+    y:0
+};
 
 canvas.addEventListener('mousemove', (event) => {
     // Get the canvas position relative to the viewport
     const rect = canvas.getBoundingClientRect();
     
     // Calculate the mouse position relative to the canvas
-    mouseX = event.clientX - rect.left;
-    mouseY = event.clientY - rect.top;
+        mouse.x = event.clientX - rect.left,
+        mouse.y = event.clientY - rect.top
     
-    // Update the display with the calculated coordinates
-    coords.textContent = `Mouse Coordinates: (${mouseX}, ${mouseY})`;
-    console.log(mouseX,mouseY)
+    
+    coords.textContent = `Mouse Coordinates: (${mouse.x}, ${mouse.y})`;
+    console.log(mouse.x,mouse.y)
 });
 
 
 
 
-function randomNumberBetweenExcludingZero(){
+function randomNumberBetweenExcludingZero() {
     let x = 0; 
     let y = 0; 
-    while (x == 0 && y == 0){
-        x = Math.floor(Math.random() * 7) - 3;
-        y = Math.floor(Math.random() * 7) - 3;
+    while (x === 0 && y === 0) {
+        x = (Math.random() * 0.6) - 0.3; // Generates between -0.2 and 0.2
+        y = (Math.random() * 0.6) - 0.3; // Generates between -0.2 and 0.2
     }
-    return [x,y];
+    return [x, y];
 }
+
 
 function createNode(){
     let [dx,dy] = randomNumberBetweenExcludingZero();
+    let x = Math.floor(Math.random()*canvas.width-1)
+    let y = Math.floor(Math.random()*canvas.height-1)
     return {
         radius: 1,
         dx: dx,
         dy: dy,
-        x: Math.floor(Math.random()*canvas.width-1),
-        y: Math.floor(Math.random()*canvas.height-1),
+        x: x,
+        y: y,
+        actualX: x,
+        actualY: y,
+
         move: function(){
-            this.x += this.dx;
-            this.y += this.dy;
+            this.actualX += this.dx;
+            this.actualY += this.dy;
+            this.x = Math.floor(this.actualX);
+            this.y = Math.floor(this.actualY);
             if (this.x+this.radius >= canvas.width || this.x - this.radius <= 0) {
                 this.dx = -this.dx
             }
@@ -65,16 +76,33 @@ function createNode(){
     }
 }
 
+function distBetweenPoints(obj1, obj2) {
+    let num1 = Math.pow(obj1.x - obj2.x, 2); // (x2 - x1)^2
+    let num2 = Math.pow(obj1.y - obj2.y, 2); // (y2 - y1)^2
+    let distance = Math.sqrt(num1 + num2);
+    return distance;
+}
+
+function drawBetween(obj1,obj2,alpha){
+    
+    ctx.beginPath();
+    ctx.moveTo(obj1.x,obj1.y)
+    ctx.lineTo(obj2.x,obj2.y);
+    ctx.strokeStyle = `rgba(0, 149, 221, ${alpha})`;
+    ctx.stroke();
+}
+
 nodes = [];
 
-for (let i = 0; i < 400; i++){
+for (let i = 0; i < 200; i++){
     nodes.push(createNode())
 }
 
 
 // function to update the canvas
 function update(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
     for (let i = 0; i < nodes.length; i++){
         nodes[i].move()
         ctx.beginPath();
@@ -83,11 +111,27 @@ function update(){
         ctx.fill()
         ctx.closePath()
     }
+    closeNodes = [];
+    for (let i = 0; i < nodes.length; i++){
+        if (distBetweenPoints(mouse,nodes[i]) < 490) {
+            closeNodes.push(nodes[i])
+        }
+    }
 
-    
+    for (let i = 0; i < closeNodes.length; i++){
+        for (let j = 0; j < closeNodes.length; j++){
+            let distance1 = distBetweenPoints(closeNodes[i],closeNodes[j]);
+            if (distance1<150 && j!=i){
+                let distance2 = distBetweenPoints(closeNodes[j],mouse);
+                let alpha = 1 - distance2/490
 
-
-
+                drawBetween(closeNodes[i],closeNodes[j],alpha);
+            }
+        }
+        
+    }
     requestAnimationFrame(update);
 }
+
+
 update();
